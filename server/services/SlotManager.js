30b4c34e49f4f6882ae3c6d67b0b4d36392d92ef
Igor Vaisman <dentalon599@gmail.com>
2024-04-27 04:13:03 +0400

    fixed slots booking

--- server/services/SlotManager.js
+++ server/services/SlotManager.js
@@ -6,43 +6,50 @@ const moment = require('moment-timezone');
 class SlotManager {
     constructor(doctorId) {
         this.doctorScheduleManager = new DoctorScheduleManager(doctorId);
-        this.appointmentManager = new AppointmentManager();
     }
 
     async generateSlots(date, timezone = 'Asia/Yerevan') {
-        const dayOfWeek = moment.tz(date, timezone).day();
+        const dayOfWeek = moment(date).day();
         const schedule = await this.doctorScheduleManager.getSchedule();
 
         if (!schedule) {
             return []; // Если день заблокирован или расписание не найдено
         }
-        
+
         const workHours = schedule.days.find(d => d.dayOfWeek === dayOfWeek);
         if (!workHours) {
             return [];
         }
-        
+
         let slots = [];
         let startTime = moment(`${date}T${workHours.startTime}`);
         let endTime = moment(`${date}T${workHours.endTime}`);
         
-        while (startTime.isBefore(endTime)) {
-            let endTimeSlot = startTime.clone().add(1, 'hour'); // Добавляем 1 час
+        let currentTime = startTime;
 
-            slots.push({ startTime: startTime, endTime: endTimeSlot });
-            startTime = endTimeSlot;
+        while (currentTime.isBefore(endTime)) {
+            let endTimeSlot = currentTime.clone().add(1, 'hour'); // Добавляем 1 час
+
+            slots.push({
+                startTime: currentTime.toDate(),
+                endTime: endTimeSlot.toDate()
+            });
+            currentTime = endTimeSlot;
         }
-
-        // Фильтрация уже забронированных слотов
-        const appointments = await Appointment.find({
-            date: moment(date).startOf('day').toDate(),
-            startTime: startTime.date(),
-            doctorId: 1  // Пока что статический ID
-        });
         
-        slots = slots.filter(slot => !appointments.some(app => moment(app.startTime).format() === slot.startTime.toISOString()));
-        // slots = slots.map(slot => ({startTime: moment.tz(slot.startTime, timezone), endTime: moment.tz(slot.endTime, timezone)}));
-        return slots;
+        // Фильтрация уже забронированных слотов
+        const busySlots = await Appointment.find({
+            $and: [
+                { startTime: { $gte: startTime } },
+                { endTime: { $lte: endTime } }
+            ],
+            doctorId: 1,
+        }).lean(); // Преобразование в JS объекты для уменьшения нагрузки
+
+        // Фильтрация слотов на стороне сервера
+        return slots.filter(slot => !busySlots.some(busy =>
+            (busy.startTime < slot.endTime && busy.endTime > slot.startTime)
+        ));
     }
 }
 
